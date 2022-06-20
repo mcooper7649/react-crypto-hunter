7a1f78e9acf5d2e0620aad9851870abf97ee8ab9
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-06-20 11:14:39 -0400

    added login.js handleSubmit logic

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -1,11 +1,45 @@
 import React, { useState } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import { Box, Button, TextField } from '@material-ui/core';
+import { CryptoState } from '../../CryptoContext';
+import { auth } from '../../firebase';
 
 const Login = ({ handleClose }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = () => {};
+  const { setAlert } = CryptoState();
+
+  const handleSubmit = async () => {
+    if (!email || !password) {
+      setAlert({
+        open: true,
+        message: 'Please fill all the Fields',
+        type: 'error',
+      });
+      return;
+    }
+    try {
+      const result = await signInWithEmailAndPassword(auth, email, password);
+
+      console.log(result);
+
+      setAlert({
+        open: true,
+        message: `Login Successful. Welcome ${result.user.email}`,
+        type: 'success',
+      });
+
+      handleClose();
+    } catch (error) {
+      setAlert({
+        open: true,
+        message: error.message,
+        type: 'error',
+      });
+      return;
+    }
+  };
 
   return (
     <Box
