1633a766bc884a58c6b864cc6b196f12a4846ae0
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-06-22 09:33:30 -0400

    completed add user authentication and watchlist with firebase

--- src/components/Authentication/UserSideBar.js
+++ src/components/Authentication/UserSideBar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import Button from '@material-ui/core/Button';
+import { Avatar, Button } from '@material-ui/core';
 import { CryptoState } from '../../CryptoContext';
-import { Avatar } from '@material-ui/core';
 import { signOut } from 'firebase/auth';
-import { auth } from '../../firebase';
+import { auth, db } from '../../firebase';
+import { numberWithCommas } from '../CoinsTable';
+import { AiFillDelete } from 'react-icons/ai';
+import { doc, setDoc } from 'firebase/firestore';
 
 const useStyles = makeStyles({
   container: {
@@ -24,18 +26,18 @@ const useStyles = makeStyles({
     gap: '20px',
     height: '92%',
   },
+  logout: {
+    height: '8%',
+    width: '100%',
+    backgroundColor: '#EEBC1D',
+    marginTop: 20,
+  },
   picture: {
     width: 200,
     height: 200,
     cursor: 'pointer',
     backgroundColor: '#EEBC1D',
     objectFit: 'contain',
-  },
-  logout: {
-    height: '8%',
-    width: '100%',
-    backgroundColor: '#EEBC1D',
-    marginTop: 20,
   },
   watchlist: {
     flex: 1,
@@ -50,15 +52,27 @@ const useStyles = makeStyles({
     gap: 12,
     overflowY: 'scroll',
   },
+  coin: {
+    padding: 10,
+    borderRadius: 5,
+    color: 'black',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#EEBC1D',
+    boxShadow: '0 0 3px black',
+  },
 });
 
-export default function TemporaryDrawer() {
+export default function UserSidebar() {
   const classes = useStyles();
   const [state, setState] = React.useState({
     right: false,
   });
+  const { user, setAlert, watchlist, coins, symbol } = CryptoState();
 
-  const { user, setAlert } = CryptoState();
+  console.log(watchlist, coins);
 
   const toggleDrawer = (anchor, open) => (event) => {
     if (
@@ -76,9 +90,33 @@ export default function TemporaryDrawer() {
     setAlert({
       open: true,
       type: 'success',
-      message: 'Logout Successfull!',
+      message: 'Logout Successfull !',
     });
+
     toggleDrawer();
+  };
+
+  const removeFromWatchlist = async (coin) => {
+    const coinRef = doc(db, 'watchlist', user.uid);
+    try {
+      await setDoc(
+        coinRef,
+        { coins: watchlist.filter((wish) => wish !== coin?.id) },
+        { merge: true }
+      );
+
+      setAlert({
+        open: true,
+        message: `${coin.name} Removed from the Watchlist !`,
+        type: 'success',
+      });
+    } catch (error) {
+      setAlert({
+        open: true,
+        message: error.message,
+        type: 'error',
+      });
+    }
   };
 
   return (
@@ -86,11 +124,11 @@ export default function TemporaryDrawer() {
       {['right'].map((anchor) => (
         <React.Fragment key={anchor}>
           <Avatar
-            className={classes.picture}
             onClick={toggleDrawer(anchor, true)}
             style={{
               height: 38,
               width: 38,
+              marginLeft: 15,
               cursor: 'pointer',
               backgroundColor: '#EEBC1D',
             }}
@@ -120,11 +158,29 @@ export default function TemporaryDrawer() {
                 >
                   {user.displayName || user.email}
                 </span>
-              </div>
-              <div className={classes.watchlist}>
-                <span style={{ fontSize: 15, textShadow: '0 0 5px black' }}>
-                  Watchlist
-                </span>
+                <div className={classes.watchlist}>
+                  <span style={{ fontSize: 15, textShadow: '0 0 5px black' }}>
+                    Watchlist
+                  </span>
+                  {coins.map((coin) => {
+                    if (watchlist.includes(coin.id))
+                      return (
+                        <div className={classes.coin}>
+                          <span>{coin.name}</span>
+                          <span style={{ display: 'flex', gap: 8 }}>
+                            {symbol}{' '}
+                            {numberWithCommas(coin.current_price.toFixed(2))}
+                            <AiFillDelete
+                              style={{ cursor: 'pointer' }}
+                              fontSize="16"
+                              onClick={() => removeFromWatchlist(coin)}
+                            />
+                          </span>
+                        </div>
+                      );
+                    else return <></>;
+                  })}
+                </div>
               </div>
               <Button
                 variant="contained"
